b48ea6c2b33fb68b3e59e65e13c278047891541c
sawwere <sawer_13@mail.ru>
2021-11-03 18:53:49 +0300

    merge Menu branch

--- Assets/Scripts/UI/Menu/MenuControls.cs
+++ Assets/Scripts/UI/Menu/MenuControls.cs
@@ -7,7 +7,7 @@ public class MenuControls : MonoBehaviour
 {
     public void PlayPressed()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 
     public void ExitPressed()
