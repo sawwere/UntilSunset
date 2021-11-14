1942c48d26f8470710d62027159f1fc38ff2e380
MovchanGitHub <92666028+MovchanGitHub@users.noreply.github.com>
2021-11-14 21:53:46 +0300

    Animations Hot Fix
    Enemies animations bugs fixes

--- Assets/Scripts/EnemyScript/EnemyClose.cs
+++ Assets/Scripts/EnemyScript/EnemyClose.cs
@@ -8,15 +8,23 @@ public class EnemyClose : EnemyCharacter
 
     public void DoDamage(Collider2D collision)
     {
+        hitTimer -= Time.deltaTime;
+        //immunityTimer -= Time.deltaTime;
+
         Building b = collision.GetComponent<Building>();
         if (b != null)
         {
             if (hitTimer <= 0)
+            {
                 b.RecieveDamage(this.damage);
 
-            hitTimer = hitPeriod;
-
-            animator.Play("Hit");
+                hitTimer = hitPeriod;
+            }
+            else if (hitTimer <= 1.25f)
+            {
+                animator.Play("Hit");
+            }
+            else animator.Play("Idle");
         }
     }
 }
