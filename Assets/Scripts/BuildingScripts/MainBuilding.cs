a5f26614b61b8c1fead8414c8606291449a3b1f9
sawwere <sawer_13@mail.ru>
2021-11-23 20:18:35 +0300

    Update MainBuilding

--- Assets/Scripts/BuildingScripts/MainBuilding.cs
+++ Assets/Scripts/BuildingScripts/MainBuilding.cs
@@ -2,34 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainBuilding : Building
+public class MainBuilding : MonoBehaviour
 {
-    
-    protected override void Start()
-    {
-        maxHealth = 3;
-        base.Start();
-    }
-
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         EnemyCharacter e = collision.gameObject.GetComponent<EnemyCharacter>();
         if (e)
         {
             e.EnterMainBuilding();
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-
-        EnemyCharacter e = collision.gameObject.GetComponent<EnemyCharacter>();
-        if (e)
-        {
-            Debug.Log("base under attack");
-            RecieveDamage(e.damage);
-            UIHealthBar.instance.SetValue(health / (float)maxHealth); // устанавливает новое значение для полоски здоровья
         }
     }
 }
