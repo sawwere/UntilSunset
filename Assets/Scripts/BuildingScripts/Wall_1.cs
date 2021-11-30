bef492b2b5165e231673a0922c0a0dd9a7300d77
venexene <venexene@gmail.com>
2021-11-30 09:40:39 +0300

    Update mouse click

--- Assets/Scripts/BuildingScripts/Wall_1.cs
+++ Assets/Scripts/BuildingScripts/Wall_1.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Wall_1 : Wall
 {
     public float displayTime = 5.0f;
+    private bool upgraded = false;
     public GameObject dialogBox;
     public GameObject wall2;
     private Resources resources;
+    private BuildPlace_1 bp;
     float timerDisplay;
 
     protected override void Start()
     {
+        bp = transform.parent.GetComponent<BuildPlace_1>();
+        bp.GetComponent<BoxCollider2D>().enabled = false;
         timerDisplay = -1.0f;
         dialogBox.SetActive(false);
         maxHealth = 2;
@@ -30,12 +35,30 @@ public class Wall_1 : Wall
         }
     }
 
+    private void OnMouseDown()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            DisplayDialog();
+        }
+    }
+
+    public void OnDestroy()
+    {
+        if (!upgraded)
+        {
+            bp.GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+
     public void UpgradeWall()
     {
         resources = GameObject.Find("CoinsText").GetComponent<Resources>();
         if (GameStats.Coins >= 3)
         {
-            Instantiate(wall2, transform.position, transform.rotation);
+            upgraded = true;
+            var wall2inst = Instantiate(wall2, transform.position, transform.rotation);
+            wall2inst.transform.SetParent(transform.parent.transform);
             GameStats.Coins -= 3;
             resources.UpdateCoins();
             HideDialog();
--- Assets/Scripts/BuildingScripts/Wall_2.cs
+++ Assets/Scripts/BuildingScripts/Wall_2.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Wall_2 : Wall
 {
+
+    private BuildPlace_1 bp;
+
     protected override void Start()
     {
+        bp = transform.parent.GetComponent<BuildPlace_1>();
         maxHealth = 4;
         base.Start();
+    }
+
+    public void OnDestroy()
+    {
+        bp.GetComponent<BoxCollider2D>().enabled = true;
     }
 
     private void OnCollisionStay2D(Collision2D collision)
