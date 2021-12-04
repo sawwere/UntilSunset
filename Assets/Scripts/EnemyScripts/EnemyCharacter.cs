9202242bf4dd07a6a095734d9ff2cf68c363d470
MovchanGitHub <92666028+MovchanGitHub@users.noreply.github.com>
2021-12-04 19:04:01 +0300

    Main charater abilities(egor) (#51)

--- Assets/Scripts/EnemyScripts/EnemyCharacter.cs
+++ Assets/Scripts/EnemyScripts/EnemyCharacter.cs
@@ -9,6 +9,7 @@ public class EnemyCharacter: MonoBehaviour, IDamage
     public int price; // �������� ��������� ������
     [SerializeField] private int maxHealth = 2; //������������ ��������
     public float speed = 1.0f; //�������� ��������
+    private float speedInit;
     public int line;
     public int armor = 0; //�����
     public int damage = 1; //����
@@ -41,12 +42,14 @@ public class EnemyCharacter: MonoBehaviour, IDamage
     // Start is called before the first frame update
     void Start()
     {
+        speedInit = speed;
         line = (int)transform.position.y;
         GameStats.enemyOnScreen[line+1].Add(this);
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
         immunityTimer = 0;
         hitTimer = firstHitPeriod;
+        transform.localScale = new Vector3(transform.localScale.x * direction, transform.localScale.y, transform.localScale.x);
     }
 
     // Update is called once per frame
@@ -124,7 +127,7 @@ public class EnemyCharacter: MonoBehaviour, IDamage
     {
         direction *= -1;
         PlayWalkAnimation();
-        transform.localScale -= new Vector3(2 * transform.localScale.x, 0, 0);
+        transform.localScale = new Vector3(transform.localScale.x * direction, transform.localScale.y, transform.localScale.x);
         transform.GetChild(0).GetComponent<BoxCollider2D>().enabled = false;
     }
 
@@ -137,5 +140,23 @@ public class EnemyCharacter: MonoBehaviour, IDamage
     public int GetLine() 
     {
         return this.line;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Minion")
+        {
+            speed = 0;
+            Debug.Log("OnCollisionEnter2D Enemy");
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Minion")
+        {
+            speed = speedInit;
+            Debug.Log("OnCollisionExit2D Enemy");
+        }
     }
 }
