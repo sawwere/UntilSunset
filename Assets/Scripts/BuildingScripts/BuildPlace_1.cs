5397da6b2fbb8425cd60122b200b31f719044585
MovchanGitHub <92666028+MovchanGitHub@users.noreply.github.com>
2021-12-19 18:05:34 +0300

    Squashed commit of the following:
    commit c1dbbb5b53258feee80b1d4f29c1458b22ad15d2
    Author: venexene <venexene@gmail.com>
    Date:   Sun Dec 19 18:01:21 2021 +0300
    
        Update walls building
    
    commit 3e5b9dbb0e62f61e14287e5cdc0dc64f89443272
    Author: venexene <venexene@gmail.com>
    Date:   Wed Dec 15 19:29:03 2021 +0300
    
        Update Wall1

--- Assets/Scripts/BuildingScripts/BuildPlace_1.cs
+++ Assets/Scripts/BuildingScripts/BuildPlace_1.cs
@@ -9,6 +9,7 @@ public class BuildPlace_1 : MonoBehaviour
     public float displayTime = 5.0f;
     public GameObject dialogBox;
     public GameObject wall;
+    public static GameObject obj_struct;
     float timerDisplay;
     private Resources resources;
     private bool EnemyIsNear;
@@ -35,9 +36,21 @@ public class BuildPlace_1 : MonoBehaviour
 
     private void OnMouseDown()
     {
-        if (!EventSystem.current.IsPointerOverGameObject())
+        if (!EventSystem.current.IsPointerOverGameObject() && (obj_struct != null))
         {
-            DisplayDialog();
+            BuildStrut();
+        }
+    }
+
+    public void BuildStrut()
+    {
+        if ((GameStats.Wood >= 3) && (!EnemyIsNear))
+        {
+            var structinst = Instantiate(obj_struct, new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z), transform.rotation);
+            structinst.transform.SetParent(this.transform);
+            GameStats.Wood -= 3;
+            resources.UpdateWood();
+            HideDialog();
         }
     }
 
