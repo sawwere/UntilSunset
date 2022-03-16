31b0ca3b50d436e4fdee29ea9dd600810a4808f3
your-panacea <73231241+your-panacea@users.noreply.github.com>
2022-03-16 19:22:28 +0300

    Squashed commit of the following:
    commit 3e6b525c523924edaee8b558f036f512940b8a23
    Author: your-panacea <73231241+your-panacea@users.noreply.github.com>
    Date:   Wed Mar 16 19:21:58 2022 +0300
    
        Fix bugs and add Settings Menu

--- Assets/Scripts/UI/Menu/MenuControls.cs
+++ Assets/Scripts/UI/Menu/MenuControls.cs
@@ -5,11 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class MenuControls : MonoBehaviour
 {
+    public GameObject Play;
+    public GameObject Level1;
+    public GameObject Level2;
+    public GameObject Tutorial;
     public void PlayPressed()
+    {
+        Play.SetActive(false);
+        Level1.SetActive(true);
+        Level2.SetActive(true);
+        Tutorial.SetActive(true);
+    }
+
+    public void Level1Pressed()
     {
         SceneManager.LoadScene("Level_1_Scene");
     }
 
+    public void Level2Pressed()
+    {
+        SceneManager.LoadScene("Level_2_Scene");
+    }
+
+    public void TutorialPressed()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
     public void ExitPressed()
     {
         Application.Quit();
--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,42 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public static bool GameIsLosed= false;
+    public static bool GameIsWin = false;
     public GameObject pauseMenuUI;
     public GameObject loseMenuUI;
     public GameObject winMenuUI;
- 
+
+    public bool SettingsIsOpened = false; //Открыто ли меню настроек
+    public float volume = 0; //Громкость
+    public int quality = 0; //Качество
+    public bool isFullscreen = false; //Полноэкранный режим
+    public AudioMixer audioMixer; //Регулятор громкости
+    public Dropdown resolutionDropdown; //Список с разрешениями для игры
+    private Resolution[] resolutions; //Список доступных разрешений
+    private int currResolutionIndex = 0; //Текущее разрешение
+    public GameObject SettingsMenu;
 
 
-    private void Start()
+
+    private void Awake()
     {
         GameIsLosed = false;
         GameIsPaused = false;
+        GameIsWin = false;
+        SettingsIsOpened = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && !GameIsLosed)
+        if (Input.GetKeyDown(KeyCode.Escape) && !GameIsLosed && !GameIsWin)
         {
                 if (GameIsPaused)
                 {
-                for (int i = 0; i <pauseMenuUI.transform.childCount; i++)
-                {
-                    GameObject child = pauseMenuUI.transform.GetChild(i).gameObject;
-                    if (child.CompareTag("Button"))
+                    if (SettingsIsOpened)
                     {
-                        Animator animator = child.GetComponent<Animator>();
-                        animator.CrossFade("Normal", 0f);
-                        animator.Update(0f);
+                        BackToPauseMenu();
                     }
-                }
-                Resume();
+                    else
+                        Resume();
                 }
                 else
                 {
@@ -90,7 +101,45 @@ public class PauseMenu : MonoBehaviour
     {
         Time.timeScale = 0f;
         winMenuUI.SetActive(true);
+        GameIsWin = true;
     }
 
- 
+    // =======================================================================
+    // Меню настроек
+    // =======================================================================
+
+    public void SettingsPressed()
+    {
+        pauseMenuUI.SetActive(false);
+        SettingsMenu.SetActive(true);
+        SettingsIsOpened = true;
+    }
+
+    public void BackToPauseMenu()
+    {
+        pauseMenuUI.SetActive(true);
+        SettingsMenu.SetActive(false);
+        SettingsIsOpened = false;
+    }
+
+    public void ChangeVolume(float val) 
+    {
+        volume = val;
+    }
+
+    public void ChangeResolution(int index) 
+    {
+        currResolutionIndex = index;
+    }
+
+    public void ChangeFullscreenMode(bool val) 
+    {
+        isFullscreen = val;
+    }
+
+    public void ChangeQuality(int index) 
+    {
+        quality = index;
+    }
+
 }
