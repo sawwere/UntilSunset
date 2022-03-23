427d4f68f74ed78e70cb28721f543858a4148da4
MovchanGitHub <92666028+MovchanGitHub@users.noreply.github.com>
2022-03-23 21:24:25 +0300

    Scene updates

--- Assets/Scripts/MerchantScripts/ExchangeScript.cs
+++ Assets/Scripts/MerchantScripts/ExchangeScript.cs
@@ -8,16 +8,39 @@ public class ExchangeScript : MonoBehaviour
     private Resources CoinsRes;
     private Resources WoodRes;
     private Resources StoneRes;
-    private Resources HenchmanRes;
 
     // Start is called before the first frame update
     private void Start()
     {
-        CoinsRes= GameObject.Find("CoinsText").GetComponent<Resources>();
+        CoinsRes = GameObject.Find("CoinsText").GetComponent<Resources>();
         WoodRes = GameObject.Find("WoodText").GetComponent<Resources>();
         StoneRes = GameObject.Find("StoneText").GetComponent<Resources>();
-        HenchmanRes = GameObject.Find("HenchmenText").GetComponent<Resources>();
     }
+
+    public void StoneToCoinButtonPressed()
+    {
+        if (GameStats.Stone >= 1)
+        {
+            GameStats.Stone -= 1;
+            GameStats.Coins += 1;
+            StoneRes.UpdateStones();
+            CoinsRes.UpdateCoins();
+            EventSystem.current.GetComponent<EventSystem>().SetSelectedGameObject(null);
+        }
+    }
+
+    public void CoinToStoneButtonPressed()
+    {
+        if (GameStats.Coins >= 3)
+        {
+            GameStats.Coins -= 3;
+            GameStats.Stone += 1;
+            CoinsRes.UpdateCoins();
+            StoneRes.UpdateStones();
+            EventSystem.current.GetComponent<EventSystem>().SetSelectedGameObject(null);
+        }
+    }
+
     public void WoodToCoinButtonPressed()
     {
         if (GameStats.Wood >= 3)
@@ -32,36 +55,12 @@ public class ExchangeScript : MonoBehaviour
 
     public void CoinToWoodButtonPressed()
     {
-        if (GameStats.Coins >= 1)
+        if (GameStats.Coins >= 3)
         {
-            GameStats.Coins -= 1;
-            GameStats.Wood += 1;
+            GameStats.Coins -= 3;
+            GameStats.Wood += 5;
             CoinsRes.UpdateCoins();
             WoodRes.UpdateWood();
-            EventSystem.current.GetComponent<EventSystem>().SetSelectedGameObject(null);
-        }
-    }
-
-    public void StoneToCoinButtonPressed()
-    {
-        if (GameStats.Stone >= 3)
-        {
-            GameStats.Stone -= 3;
-            GameStats.Coins += 2;
-            CoinsRes.UpdateCoins();
-            StoneRes.UpdateStones();
-            EventSystem.current.GetComponent<EventSystem>().SetSelectedGameObject(null);
-        }
-    }
-
-    public void BuyBatButtonPressed()
-    {
-        if (GameStats.Coins >= 2)
-        {
-            GameStats.Coins -= 2;
-            GameStats.Henchman += 1;
-            CoinsRes.UpdateCoins();
-            HenchmanRes.UpdateHenchman();
             EventSystem.current.GetComponent<EventSystem>().SetSelectedGameObject(null);
         }
     }
