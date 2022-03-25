609f915ab92f57b4396c2f5ff4ad860ba67ab2ae
sawwere <81819674+sawwere@users.noreply.github.com>
2022-03-25 18:53:23 +0300

    Menu settings interface (#106)
    * Add new menu, change buttons
    
    * Squashed commit of the following:
    
    commit fc07cbf02c0f2adec269a6a1374f5a39bafb173b
    Author: ga12lil <agrigoren@sfedu.ru>
    Date:   Thu Mar 24 22:05:29 2022 +0300
    
        Done!
    
    commit 1e00b2c0ac93a62ee197f919b82a18ea1db23580
    Author: ga12lil <agrigoren@sfedu.ru>
    Date:   Thu Mar 24 17:08:48 2022 +0300
    
        Update Menu.unity
    
    commit 634f70de889736534778e209985ecd9b2df435f6
    Author: ga12lil <agrigoren@sfedu.ru>
    Date:   Thu Mar 24 01:47:22 2022 +0300
    
        add FAQ in menu
    
    commit b9de26a6971ea6ed441e2a639496a24c321ff77b
    Author: ga12lil <agrigoren@sfedu.ru>
    Date:   Wed Mar 23 18:10:14 2022 +0300
    
        fix end of game
    
        now only 3days
    
    * change pause-menu and main-menu
    
    * Revert Artem branch merge
    
    Co-authored-by: your-panacea <73231241+your-panacea@users.noreply.github.com>

--- Assets/Scripts/UI/Menu/MenuControls.cs
+++ Assets/Scripts/UI/Menu/MenuControls.cs
@@ -5,18 +5,45 @@ using UnityEngine.SceneManagement;
 
 public class MenuControls : MonoBehaviour
 {
+    public GameObject Exit;
+    public GameObject Back;
     public GameObject Play;
     public GameObject Level1;
     public GameObject Level2;
     public GameObject Level3;
     public GameObject Tutorial;
+<<<<<<< HEAD
+    public GameObject InfoBut;
+    public GameObject ScrollInfo;
+    public Text MenuLogo;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !Play.activeInHierarchy && !ScrollInfo.activeInHierarchy)
+        {
+            BackToMain();
+        }
+
+    }
+
+
+
+
+=======
+>>>>>>> parent of e57661d (Squashed commit of the following:)
     public void PlayPressed()
     {
+        Exit.SetActive(false);
         Play.SetActive(false);
         Level1.SetActive(true);
         Level2.SetActive(true);
         Level3.SetActive(true);
         Tutorial.SetActive(true);
+<<<<<<< HEAD
+        InfoBut.SetActive(false);
+        Back.SetActive(true);
+=======
+>>>>>>> parent of e57661d (Squashed commit of the following:)
     }
 
     public void Level1Pressed()
@@ -43,4 +70,33 @@ public class MenuControls : MonoBehaviour
         Application.Quit();
         Debug.Log("Exit pressed!");
     }
+<<<<<<< HEAD
+    public void OpenInfo()
+    {
+        Play.SetActive(false);
+        InfoBut.SetActive(false);
+        ScrollInfo.SetActive(true);
+        MenuLogo.text = "Îá Èãðå";
+    }
+    public void CloseInfo()
+    {
+        Play.SetActive(true);
+        InfoBut.SetActive(true);
+        ScrollInfo.SetActive(false);
+        MenuLogo.text = "Ìåíþ";
+    }
+
+    public void BackToMain()
+    {
+        Exit.SetActive(true);
+        Play.SetActive(true);
+        Level1.SetActive(false);
+        Level2.SetActive(false);
+        Level3.SetActive(false);
+        Tutorial.SetActive(false);
+        InfoBut.SetActive(true);
+        Back.SetActive(false);
+    }
+=======
+>>>>>>> parent of e57661d (Squashed commit of the following:)
 }
