af893598628aa50cec2b5f30a0fd8ec2cd3083b6
your-panacea <73231241+your-panacea@users.noreply.github.com>
2022-04-02 18:39:05 +0300

    Fix Volume settings

--- Assets/Scripts/UI/Menu/VolumeControls.cs
+++ Assets/Scripts/UI/Menu/VolumeControls.cs
@@ -2,15 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
-public class SettingsMenu : MonoBehaviour
+public class VolumeControls : MonoBehaviour
 {
    public AudioMixer audioMixer;
+   public string volumeParameter = "MasterVolume";
+   public Slider slider;
 
-    public void SetVolume (float volume)
+    private const float _multiplier = 20f;
+
+    private void Awake()
     {
-        audioMixer.SetFloat("volume", volume);
+        slider.onValueChanged.AddListener(HandleSliderVallueChanged);
     }
-
-
+    private void HandleSliderVallueChanged(float value)
+    {
+        var volumeValue = Mathf.Log10(value) * _multiplier;
+        audioMixer.SetFloat(volumeParameter, volumeValue);
+    }
+  
 }
