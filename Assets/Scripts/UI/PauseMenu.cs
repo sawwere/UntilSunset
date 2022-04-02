8756f3f70ab9a579357be9bf2a85bad23421411a
Apetrosia <92916916+Apetrosia@users.noreply.github.com>
2022-04-02 18:40:44 +0300

    Music and sounds (#126)
    * Added Sound Squares
    
    * Add walking and collecting sounds
    
    Sand, home, stonewalk, stone, wood
    
    * Fixed bugs with sounds' scripts
    
    * Added new soundtracks and fixed resources' sounds
    
    * Added buildings' sounds
    
    Build and recover
    
    * Update Towers
    
    * Update Damage
    
    * Update BuildingUI
    
    * Update Damage
    
    * Adding Buttons Sprites
    
    * Added GrassWalk sound and other sounds
    
    * Added walls' sounds
    
    * Added sound for uncollectible resources
    
    And improved some sounds
    
    * A plug is installed
    
    * Added wall's upgrade's sound
    
    * Added tutorial sundtrack and fixed bugs with walk  sound
    
    * Fixed bug with tutorial music
    
    * Update Buttons
    
    * Tutorial Scene Extension
    
    * Fix Tutorial Building
    
    * Fix TutorialUI
    
    * Add Volume Settings
    
    * Fix player's and resources' sounds' bug in tutorial
    
    * Add day soundtrack for 2 level and fix 2 level's enemies' walk sounds
    
    * Fix some bugs with sounds
    
    * Fix bug with walls' sound
    
    * Improve volume of OSTs' audio files
    
    * Add new music script
    
    Two new soundtracks are already in the game! (in 1 level)
    
    * Delete wrong mp3 file
    
    * Fix bugs with walk sounds in tutorial
    
    * Add Merchant's coin sound and fix bug with music continuing after winning
    
    Co-authored-by: venexene <venexene@gmail.com>
    Co-authored-by: MovchanGitHub <92666028+MovchanGitHub@users.noreply.github.com>
    Co-authored-by: your-panacea <73231241+your-panacea@users.noreply.github.com>

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -95,6 +95,11 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 0f;
         winMenuUI.SetActive(true);
         GameIsWin = true;
+        GameObject.Find("Player").GetComponent<PlayerController>().PauseWalkSound();
+        foreach (var e in GameObject.FindGameObjectsWithTag("Enemy"))
+            e.GetComponent<EnemyCharacter>().PauseWalkSound();
+        foreach (var e in GameObject.FindGameObjectsWithTag("Friend"))
+            e.GetComponent<EnemyCharacter>().PauseWalkSound();
     }
     public void Level1Pressed()
     {
