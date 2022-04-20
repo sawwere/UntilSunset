06a5853da43c7f983850934e8f3d80dc38089a1a
MovchanGitHub <92666028+MovchanGitHub@users.noreply.github.com>
2022-04-20 21:03:38 +0300

    Fix Resourse Indicator

--- Assets/Scripts/ResourseCollectable/Stump.cs
+++ Assets/Scripts/ResourseCollectable/Stump.cs
@@ -9,7 +9,7 @@ public class Stump : Tree
     protected override void ObjectDie()
     {
         tree.anim.Play("Delete");
-        Invoke(nameof(TurnToTree), Random.Range(110, 130));
+        Invoke(nameof(TurnToTree), Random.Range(90, 115));
     }
 
     public override void TurnToTree()
