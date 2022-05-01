5e8eefd575d032b7f325117661149a8e45a22843
venexene <92878000+venexene@users.noreply.github.com>
2022-05-01 20:43:27 +0300

    Health bars (#162)
    * Add RightClick
    
    * Update MerchantButtons
    
    * Update BuildingHP
    
    * Fix Towers
    
    * Add RightClickDestruction
    
    * Add HealthBars
    
    * Fix HealthBars
    
    * Fix MerchantUI
    
    * Fix RightClick
    
    * Update Coffin

--- Assets/Scripts/BuildingScripts/CoffinButton.cs
+++ Assets/Scripts/BuildingScripts/CoffinButton.cs
@@ -1,14 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class CoffinButton : MonoBehaviour
 {
     private Coffin cof;
+    private Resources resources;
+
+    public void Start()
+    {
+        resources = GameObject.Find("CoinsText").GetComponent<Resources>();
+    }
 
     public void CoffinButtonPressed()
     {
         cof = transform.parent.GetComponent<Coffin>();
         cof.Recover();
+    }
+
+    public void EnterRecoverButton(Button b)
+    {
+        resources.SetPriceMerchant(0, 0, 5);
+        resources.UpdateAll();
+    }
+
+    public void ExitRecoverButton(Button b)
+    {
+        resources.ClearPriceOrRefund();
+        resources.UpdateAll();
     }
 }
