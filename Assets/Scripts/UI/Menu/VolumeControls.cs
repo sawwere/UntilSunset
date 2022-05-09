b9d5ec4d918c9bd60b942c0521a68172a6429265
ga12lil <agrigoren@sfedu.ru>
2022-05-09 20:06:11 +0300

    Saves volume

--- Assets/Scripts/UI/Menu/VolumeControls.cs
+++ Assets/Scripts/UI/Menu/VolumeControls.cs
@@ -9,17 +9,35 @@ public class VolumeControls : MonoBehaviour
    public AudioMixer audioMixer;
    public string volumeParameter = "MasterVolume";
    public Slider slider;
+    public bool isCameraScript = false; 
 
     private const float _multiplier = 20f;
 
     private void Awake()
     {
-        slider.onValueChanged.AddListener(HandleSliderVallueChanged);
+        if (!isCameraScript)
+        {
+            slider.onValueChanged.AddListener(HandleSliderVallueChanged);
+            if (PlayerPrefs.HasKey(volumeParameter))
+            {
+                slider.value = PlayerPrefs.GetFloat(volumeParameter);
+            }
+        }
+        else
+        {
+            if (PlayerPrefs.HasKey(volumeParameter))
+            {
+                var volumeValue = Mathf.Log10(PlayerPrefs.GetFloat(volumeParameter)) * _multiplier;
+                audioMixer.SetFloat(volumeParameter, volumeValue);
+            }
+        }
+        
     }
     private void HandleSliderVallueChanged(float value)
     {
         var volumeValue = Mathf.Log10(value) * _multiplier;
         audioMixer.SetFloat(volumeParameter, volumeValue);
+        PlayerPrefs.SetFloat(volumeParameter, slider.value);
     }
   
 }
